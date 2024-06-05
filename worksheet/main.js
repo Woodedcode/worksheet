8349c0978f39740749fd8244e9db7c12435c83ce
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-06-05 15:11:12 -0500

    Update main.js

--- worksheet/main.js
+++ worksheet/main.js
@@ -94,3 +94,19 @@
 // }
 
 // fizzBuzz()
+
+// function fizzBuzz() {
+//     for (let i = 0; i < 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0){
+//             console.log('FIZZBUZZ')
+//         } else if (i % 5 === 0) {
+//             console.log('BUZZ')
+//         } else if (i % 3 === 0) {
+//             console.log('FIZZ')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// fizzBuzz()
