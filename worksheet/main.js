9ef1da7a80f9baaf150c6c0af743f6de7ef10b27
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-06-05 14:57:17 -0500

    Add files via upload

---
+++ worksheet/main.js
@@ -0,0 +1,96 @@
+// var tvShows = [
+//     'The Walking Dead',
+//     'Breaking Bad',
+//     'The Office'
+// ]
+
+// var userSavedMovies = []
+
+// // for (var i = 0; i < tvShows.length; i++) {
+// //     console.log(tvShows[i])
+// // }
+
+// var completedItems = []
+
+// var toDos = [
+//     {title: 'item 1', completed: true},
+//     {title: 'item 2', completed: false},
+//     {title: 'item 3', completed: true},
+//     {title: 'item 4', completed: false},
+//     {title: 'item 5', completed: true},
+// ]
+
+// console.log(toDos)
+
+// for (var i = 0; i < toDos.length; i++) {
+//     if (toDos[i].completed) {
+//         completedItems.push(toDos[i])
+//     }
+// }
+
+// console.log(toDos)
+// console.log(completedItems)
+
+// //WHILE LOOP
+
+// var bottles = 99
+
+// while (bottles > 0) {
+//     if (bottles !== 1) {
+//         console.log(bottles + " bottles of stuff on the wall")
+//     } else {
+//         console.log(bottles + " bottle of stuff on the wall")
+//     }
+//     bottles -= 1 
+// }
+
+// console.log(bottles)
+
+// //FIZZBUZZ
+
+// function fizzBuzz() {
+//     for (let i = 0; i <= 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log('FIZZBUZZ')
+//         } else if (i % 5 === 0){
+//             console.log('BUZZ')
+//         } else if (i % 3 === 0){
+//             console.log('FIZZ')
+//         } else{
+//             console.log(i)
+//         }
+// }
+// }
+// fizzBuzz()
+
+// function fizzBuss() {
+//     for (let i = 0; i < 100; i++){
+//         if (i % 3 === 0 && i % 5 === 0){
+//             console.log('FIZZBUSS')
+//         } else if (i % 5 === 0){
+//             console.log('BUSS')
+//         } else if (i % 3 === 0) {
+//             console.log('FIZZ')
+//         } else (
+//             console.log(i)
+//         )
+//     }
+// }
+
+// fizzBuss()
+
+// function fizzBuzz() {
+//     for (let i = 0; i < 100; i++) {
+//         if (i % 3 === 0 && i % 5 === 0){
+//             console.log('FIZZ BUZZ')
+//         } else if (i % 5 === 0) {
+//             console.log('buzz')
+//         } else if (i % 3 === 0) {
+//             console.log('fizz')
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+
+// fizzBuzz()
